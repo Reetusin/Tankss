d5b6454771959a91e666e88edf685659c86c3cce
Simona <sima.simute1999@gmail.com>
2024-11-11 18:49:44 +0200

    Level

---
+++ Assets/Scipts/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifetime = 2.0f;
+    public float speed = 15f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
