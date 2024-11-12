f104cc5bd3784062f9d7425057d6ff01a4627565
Simona <sima.simute1999@gmail.com>
2024-11-12 17:00:21 +0200

    movement

---
+++ Assets/Scrips/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
+    public float speed = 10.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var input = new Vector3();
+        input.x = Input.GetAxis("Horizontal");
+        input.z = Input.GetAxis("Vertical");
+
+        transform.position += input * speed * Time.deltaTime;
+
+        if (input != Vector3.zero)
+            transform.forward = input;
+    }
+}
